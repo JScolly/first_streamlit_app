31deb9b05421b71f5e5a9d84eac5165b0095ba62
JScolly <145778633+JScolly@users.noreply.github.com>
2023-09-22 14:53:35 +0100

    Update streamlit_app.py
    Added food to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Omega 3 & Blueberry Oatmeal
+Omega 3 & Blueberry Oatmeal,
 
-Kale, Spinach & Rocket Smoothie
+Kale, Spinach & Rocket Smoothie,
 
-Hard-Boiled Free-Range Egg
+Hard-Boiled Free-Range Egg,
+;
