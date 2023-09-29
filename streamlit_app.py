67862d1e5ab169927436fcf87f982072cd8f4b1c
JScolly <145778633+JScolly@users.noreply.github.com>
2023-09-29 13:03:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,12 +46,12 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('The user entered ', fruit_choice)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
